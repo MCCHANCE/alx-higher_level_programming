e3ee1e25e1e66fd0990b68cd240eb22f99a09c45
MCCHANCE <unwananyong2021@gmail.com>
2023-06-05 13:33:11 -0700

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-sys.exit(1)
 
+def main():
+    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+if __name__ == "__main__":
+    main()
+    sys.exit(1)
